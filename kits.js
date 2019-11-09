2e09e7738e09e1a5f4230a270105875fd2f30ffb
lvyuqing <1944124354@qq.com>
2019-11-10 00:14:18 +0800

    js工具库

---
+++ kits.js
@@ -0,0 +1,23 @@
+// 随机整数
+var kits = {};
+kits.randomInt = function (n, m) {
+    return Math.floor(Math.random() * (m - n + 1) + n);
+}
+// 生成随机rgb颜色
+kits.rgbColor = function () {
+    var r = kits.randomInt(0, 255);
+    var g = kits.randomInt(0, 255);
+    var b = kits.randomInt(0, 255);
+    var colorStr = 'rgb(' + r + ',' + g + ',' + b + ')';
+    return colorStr;
+}
+//生成随机16进制颜色
+kits.randomHexColor = function () {
+    var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    var color = ['#'];
+    for (var i = 0; i < 6; i++) {
+        var rand = Math.floor(Math.random() * arr.length);
+        color.push(arr[rand]);
+    }
+    return color.join('');
+}
